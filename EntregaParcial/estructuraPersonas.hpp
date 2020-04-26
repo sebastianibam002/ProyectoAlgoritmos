269c20c7181ad33cd6c61a0fc6c6a67a45bb2a1a
sebastianibam002 <sebastianibam@gmail.com>
2020-04-26 12:14:12 -0500

    Solucionado tema de remove ya se cambio el remove para que funcionara en el tercer caso

--- EntregaParcial/estructuraPersonas.hpp
+++ EntregaParcial/estructuraPersonas.hpp
@@ -1,6 +1,7 @@
 #ifndef _ESTRUCTURAPERSONAS_HPP
 #define _ESTRUCTURAPERSONAS_HPP
 #include <string>
+#include <iostream>
 //cajitas dentro de la estructura es decir la persona
 struct person
 {
@@ -27,37 +28,41 @@ class EstructuraPersonas
 private:
   //tenemos la raiz
   person *root;
-  //para mostrar un nodo
-  //void display_node(person* p_node, int count);
-  //insertar un nodo
-  void insert_node(person *&pPersona, int pId, std::string pLocation, bool pGender, int pAge, person*p);
-  /*
+  //para mostrar una perosna
+  void display_person(person* pPerson, int count);
+  //insertar una persona
+  void insert_person(person *&pPersona, int pId, std::string pLocation, bool pGender, int pAge, person*p);
+  
   
   //no accedemos nunca desde afuera
   //node desde donde comenzamos a destruir recursivamente
-  void destroy_recursive(BSTNode<T> *node);
-  BSTNode<T>* find_node(BSTNode<T> *node, T k);
+  
+  void destroy_recursive(person *pPerson);
+  
+  person* find_person(person *pPerson, int pId);
   //Minimo y maximo
-  BSTNode<T>* minimum(BSTNode<T> *t);
-  BSTNode<T>* maximum(BSTNode<T> *t);
-  */
+  person* minimum(person *t);
+  person* maximum(person *t);
+  
 public:
   EstructuraPersonas(){root = nullptr;}
-  //~EstructuraPersonas(){destroy_recursive(root);} //destructor 
+  ~EstructuraPersonas(){destroy_recursive(root);} //destructor 
   //crea un nodo, y derecho e izquiedo apunta a nullptr
   
-  //void display();
-  //bool empty();
+  void display();
+  bool empty();
   /*--------------*/
-  //void test(T val);
-  void insert(int pIdU, bool pGenderU, int pAgeU, std::string pLocationU){insert_node(root,pIdU, pLocationU, pGenderU,  pAgeU, nullptr);}
-  //BSTNode<T>* find(T k){return find_node(root, k);}
+  void test(int val);
+
+  //
+  void insert(int pIdU, bool pGenderU, int pAgeU, std::string pLocationU){insert_person(root,pIdU, pLocationU, pGenderU,  pAgeU, nullptr);}
+  person* find(int pId){return find_person(root, pId);}
   //Predecesor y sucesor
-  //BSTNode<T> *predecessor(BSTNode<T> *pNode); //le doy un nodo y me deberia retorna el predecesor de este
-  //BSTNode<T> *sucessor(BSTNode<T> *pNode);
+  person *predecessor(person *pPerson); //le doy un nodo y me deberia retorna el predecesor de este
+  person *successor(person *pPerson);
 
   //Removiendo Nodos
-  //BSTNode<T>* removeNode(BSTNode<T> *pNode, T k);
+  person* remove_person(person *node, int k);
 
 };
 #endif
