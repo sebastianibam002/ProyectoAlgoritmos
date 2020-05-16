5546ec8078a2b217bd352e5ebdefc172a8c0a794
sebastianibam002 <sebastianibam@gmail.com>
2020-05-16 14:36:38 -0500

    Codigo y adocumentado
    Problem

---
+++ ArchivoEntrega/main.cpp
@@ -0,0 +1,29 @@
+#include "layerNode.hpp"
+using namespace std;
+int main()
+{
+
+
+
+  BDCovid prueba;
+  prueba.generarArchivoVis(true,10,90,"Putumayo,Amazonas,Cundinamarca,Antioquia,Quindio");
+  
+  //vector<int> test = prueba.busqueda(true,19,42,"Bogota");
+  //std::cout<<"size: "<<test.size()<<std::endl;
+  /*
+  for(int i = 0; i < test.size(); i++)
+    {
+      cout<<test[i]<<" ";
+    }
+  
+  cout<<endl;
+  */
+  //std::cout<<"size: "<<test.size()<<std::endl;
+  
+  
+  
+
+  
+
+  return 0;
+}
